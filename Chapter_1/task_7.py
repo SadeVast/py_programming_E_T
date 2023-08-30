55a605a18a247afb9c37c0c8b0025daba6889fb2
SadeVast <vetalik.selikhov@mail.ru>
2023-08-30 16:10:30 +0300

    add comments

--- Chapter_1/task_7.py
+++ Chapter_1/task_7.py
@@ -1,10 +1,12 @@
-# Todo comments
+# Asking the user to enter a number
 n = int(input("Please input positive num:"))
 f = 1
 if n > 0:
+    # Adding a loop to factorial
     while (n):
         f *= n
         n -= 1
     print(f)
 else:
+    # Checking for a positive number
     print("Please input positive num")
--- Chapter_1/task_8.py
+++ Chapter_1/task_8.py
@@ -1,8 +1,8 @@
-# Todo comments
+# Asking the user to enter a number
 n = int(input("Enter the number:"))
 fib1 = fib2 = 1
 print(fib1, fib2, end=' ')
-
+# Add a loop in which we assign the sum to the variables
 for i in range(2, n):
     fib1, fib2 = fib2, fib1 + fib2
     print(fib2, end=' ')
