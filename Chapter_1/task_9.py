03a4de1c4f82dca4fb13c8d775f2084f1e9708a5
SadeVast <vetalik.selikhov@mail.ru>
2023-08-30 16:10:57 +0300

    add task_9

---
+++ Chapter_1/task_9.py
@@ -0,0 +1,11 @@
+# Create a list of numbers
+n_list = [2, 3, 10, 6, 8]
+
+# Create a function
+def sec_largest(n_list):
+    # Sort the list in ascending order and display the second-largest value
+    n_list.sort()
+    print("The second largest element of the list is:", n_list[-2])
+
+# Deriving a function
+sec_largest(n_list)
