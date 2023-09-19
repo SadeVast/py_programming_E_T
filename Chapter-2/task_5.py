458637d293ce0b2dcf3c6cd69ee185ed84fbbf31
VADESADE <vetalik.selikhov@mail.ru>
2023-09-19 16:04:26 +0300

    add task & change code

---
+++ Chapter-2/task_5.py
@@ -0,0 +1,7 @@
+num = list(input("Введите числа:"))
+b = int(input("Введите границу:"))
+out = 0
+for i in range(1, b + 1):
+    if i not in num: #TODO
+        out += i
+print(out)
---
+++ Chapter-2/tasl_4.py
@@ -0,0 +1,19 @@
+# Add a list with numbers.
+a = [2, 5, 7, 6]
+b = [5, 1, 4, 7]
+c = []
+
+for i in a:
+    for j in b:
+        if i == j:
+            c.append(i)
+            break
+print(c)
+
+if a == b:
+    print("Lists are equal.")
+elif len(a) == len(b):
+    print("Equal list size.")
+else:
+    print("Lists are not equal.")
+#TODO
